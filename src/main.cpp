21ab8c87237ffdbb7ed06c71b1f6644f3c977b13
Jose Laruta <eduardo.laruta@gmail.com>
2019-05-02 21:38:06 -0400

    debug info

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,15 @@
 using namespace cv;
 int main(int argc, char** argv )
 {
+    std::cout << "creando dispositivo \n";
     VideoCapture cap(0);
     cap.set(CAP_PROP_FRAME_WIDTH, 320);
     cap.set(CAP_PROP_FRAME_HEIGHT, 240);
-    
+    std::cout << "resolucion: 320x240 \n";
     if(!cap.isOpened()) return -1;
 
     Mat image;
+    std::cout << "leyendo imagen \n";
     cap >> image;
 
     if ( !image.data )
@@ -23,6 +25,7 @@ int main(int argc, char** argv )
         printf("No image data \n");
         return -1;
     }
+    std::cout << "guardando imagen \n";
     imwrite("cam.png", image);
     waitKey(0);
     return 0;
